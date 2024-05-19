d3d32769a972155d2c071c480917f0314cb471e0
Ahmed Saad Abd El-Kader <ahmed.saad.kader@gmail.com>
2024-05-20 01:44:00 +0300

    update

--- dist/database.js
+++ dist/database.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const pg_1 = require("pg");
 dotenv_1.default.config();
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_TEST_DB, POSTGRES_USER, POSTGRES_PASSWORD, ENV } = process.env;
+const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_TEST_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST_PROD, POSTGRES_PROD_USER, POSTGRES_PROD_DB, POSTGRES_PROD_PASSWORD, ENV } = process.env;
 console.log('ENV', ENV);
 let client = new pg_1.Pool({
     host: '',
@@ -15,6 +15,7 @@ let client = new pg_1.Pool({
     password: ''
 });
 if (ENV == 'dev') {
+    console.log('Connecting to dev database:');
     client = new pg_1.Pool({
         host: POSTGRES_HOST,
         database: POSTGRES_DB,
@@ -23,11 +24,24 @@ if (ENV == 'dev') {
     });
 }
 if (ENV == 'test') {
+    console.log('Connecting to test database');
     client = new pg_1.Pool({
         host: POSTGRES_HOST,
         database: POSTGRES_TEST_DB,
         user: POSTGRES_USER,
         password: POSTGRES_PASSWORD
+    });
+}
+if (ENV == 'production') {
+    console.log('Connecting to production database');
+    client = new pg_1.Pool({
+        host: POSTGRES_HOST_PROD,
+        database: POSTGRES_PROD_DB,
+        user: POSTGRES_PROD_USER,
+        password: POSTGRES_PROD_PASSWORD,
+        ssl: {
+            rejectUnauthorized: false
+        }
     });
 }
 exports.default = client;
